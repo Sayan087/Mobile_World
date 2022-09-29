a0e37b86a2ffa1b884720e5bba2d7c6261a16310
Sayan087 <95565159+Sayan087@users.noreply.github.com>
2022-09-29 15:36:26 +0530

    Update Contact_us.java

--- eclipse-workspace/mobile_world_automation_script/src/mobile_automation/Contact_us.java
+++ eclipse-workspace/mobile_world_automation_script/src/mobile_automation/Contact_us.java
@@ -1,26 +1,44 @@
 package mobile_automation;
 
-import java.util.Iterator;
-import java.util.Set;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class Contact_us {
-	@Test
+	@Test(dataProvider="contactus")
 	
-	public static void contact_us() {
+	public static void contact_us(String Username,String Email,String mobile,String msg) {
 				// TODO Auto-generated method stub
-			System.setProperty("webdriver.chrome.driver", "C:\\Users\\sayan.giri\\Downloads\\chromedriver_win32\\chromedriver.exe");
-			WebDriver driver=new ChromeDriver();
-			Show_pages pages=new Show_pages(driver);
-			driver.manage().window().maximize();
-			driver.get("https://mobileworld.banyanpro.com/");
-			pages.goToContact();
-			pages.goTowindow();
-			driver.quit();
+//		System.setProperty("webdriver.edge.driver", "C:\\drivers\\edgedriver_win64 (2)\\msedgedriver.exe");
+//		WebDriver driver=new EdgeDriver();
+		System.setProperty("webdriver.chrome.driver", "C:\\drivers\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		Show_pages pages=new Show_pages(driver);
+		driver.manage().window().maximize();
+		driver.get("https://qualicoach.org/mwapp/index.html");
+		pages.goToContact();
+		pages.goTowindow( Username, Email, mobile, msg);
+		driver.quit();
 		
 		
 		}
+	@DataProvider(name="contactus")
+    public Object[][] getdata()
+	{
+		 Object [][] data=new Object[2][4];
+		 data [0][0]="Sayan";
+         data [0][1]="shayan786@gmail.com";
+         data [0][2]="7063141936";
+         data [0][3]="product quality is good";
+         
+         data [1][0]="Sayan";
+         data [1][1]="sayan.giri@qualitestgroup.com";
+         data [1][2]="7866843480";
+         data [1][3]="Good Product";
+         return data;
+        
+		}
 }
+
